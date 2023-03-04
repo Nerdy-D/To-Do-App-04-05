ec67831be27f134c35c78f75abc7e14247860e83
Nerdy-D <111739562+Nerdy-D@users.noreply.github.com>
2023-03-04 22:45:29 +0200

    Basic Ui update working

--- task.js
+++ task.js
@@ -1,7 +1,22 @@
-function addTwoNumbers(a, b) {
+/*function addTwoNumbers(a, b) {
     console.log(a + b);
+*/
+// create an empty array with [] 
+var tasks = [];
 
-}
 function addTask() {
-    console.log("addingTask");
+    var newTask = document.getElementById("inputTask").value;
+    // if tasks array already has newTask show alert to user
+    tasks.push(newTask);
+    console.log("adding your new item:", newTask);
+    console.log("all of your tasks are:", tasks);
+    upDateUi();
+}
+function upDateUi() {
+    // is getting ref to where we will display our tasks
+    var container = document.getElementById("to_do_list");
+    // is creating a new task element
+    var newElement = document.createElement("div");
+    newElement.innerHTML = "hello world"
+    container.appendChild(newElement);
 }

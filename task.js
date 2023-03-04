0a3259f4a518132fcebfe0a3660898e671540b27
Nerdy-D <111739562+Nerdy-D@users.noreply.github.com>
2023-03-04 21:44:07 +0200

    Link Submit Button JS
    We are getting somewhere

--- task.js
+++ task.js
@@ -1,19 +1,7 @@
-var submitButton = document.getElementById("button1");
-var clearButton = document.getElementById("button2");
-var add = document.getElementById("write");
+function addTwoNumbers(a, b) {
+    console.log(a + b);
 
-submitButton.addEventListener("click", addStuff);
-clearButton.addEventListener("click", clear);
-
-function addStuff() {
-    todo = text1.value;
-    if (todo == "") {
-        alert("Please input data");
-    } else {
-        add.innerHTML += "<li>" + todo + "</li>";
-    }
 }
-
-function clear() {
-    add.innerHTML = "";
+function addTask() {
+    console.log("addingTask");
 }

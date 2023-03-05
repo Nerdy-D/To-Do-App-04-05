f8c51c09abe1e402402c5aed9082df0c47730c47
Nerdy-D <111739562+Nerdy-D@users.noreply.github.com>
2023-03-05 23:54:54 +0200

    Complete Task

--- task.js
+++ task.js
@@ -1,17 +1,11 @@
-/*function addTwoNumbers(a, b) {
-    console.log(a + b);
-*/
-// addTwoNumbers(4,3)
-// create an empty array with [] 
 var tasks = [];
 loadTasks();
 
 function addTask() {
     var taskName = document.getElementById("inputTask").value;
-    // if tasks array already has newTask show alert to user
     tasks.push({
         name: taskName,
-        completed: true,
+        completed: false,
     });
     console.log("adding your new item:", taskName);
     console.log("all of your tasks are:", tasks);
@@ -19,10 +13,14 @@ function addTask() {
 }
 
 function addTaskToUi(taskObj) {
-    // is getting ref to where we will display our tasks
+
     var container = document.getElementById("to_do_list");
-    // is creating a new task element
+
     var newElement = document.createElement("div");
+    newElement.addEventListener("click", () => {
+        taskObj.completed = !taskObj.completed;
+        refreshUi()
+    })
     newElement.className = "tasks";
     if (taskObj.completed) {
         newElement.className = "tasks strikethrough";
@@ -32,8 +30,7 @@ function addTaskToUi(taskObj) {
     var dueDate = document.createElement("p");
     dueDate.innerHTML = "Due by 25 March 2023";
 
-    // var calendar = document.getElementsByClassName("calendar")
-    // calendar.className = "dates"
+
     newElement.appendChild(heading);
     newElement.innerHTML += taskObj.name;
     newElement.appendChild(dueDate);
@@ -41,7 +38,6 @@ function addTaskToUi(taskObj) {
 }
 
 function removeTask() {
-    // var newTask = document.getElementById("inputTask").value;
     tasks.pop();
     refreshUi();
 }
@@ -72,12 +68,3 @@ function loadTasks() {
 
 
 
-/*function addCalendar() {
-    var addCalendar = document.createElement("dates");
-    addCalendar.className = "calendar";
-    addCalendar.innerHTML = "Due by 24 March ";
-    // addCalendar.appendChild(container);
-        var dueDate = document.getElementById("p")
-    dueDate.innerHTML = "Task to be done by 24 March 2023"
-}
-*/

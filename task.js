439d3a35a5aab31b60dbfd96fc9372b3961d2ac8
Nerdy-D <111739562+Nerdy-D@users.noreply.github.com>
2023-03-05 20:26:45 +0200

    Struggling with clear item
    trying to get the code correct to remove item

--- task.js
+++ task.js
@@ -10,13 +10,39 @@ function addTask() {
     tasks.push(newTask);
     console.log("adding your new item:", newTask);
     console.log("all of your tasks are:", tasks);
-    upDateUi();
+    upDateUi(newTask);
 }
-function upDateUi() {
+
+function upDateUi(nameOfTask) {
     // is getting ref to where we will display our tasks
     var container = document.getElementById("to_do_list");
     // is creating a new task element
     var newElement = document.createElement("div");
-    newElement.innerHTML = "hello world"
+    newElement.className = "tasks";
+    var heading = document.createElement("h4");
+    heading.innerHTML = "Audio Checklist";
+    var dueDate = document.createElement("p");
+    dueDate.innerHTML = "Due by 25 March 2023";
+
+    // var calendar = document.getElementsByClassName("calendar")
+    // calendar.className = "dates"
+    newElement.appendChild(heading);
+    newElement.innerHTML += nameOfTask;
+    newElement.appendChild(dueDate);
     container.appendChild(newElement);
 }
+
+/*function removeTask() {
+    var newTask = document.getElementById("inputTask").value;
+    tasks.pop();
+}*/
+
+/*function addCalendar() {
+    var addCalendar = document.createElement("dates");
+    addCalendar.className = "calendar";
+    addCalendar.innerHTML = "Due by 24 March ";
+    // addCalendar.appendChild(container);
+        var dueDate = document.getElementById("p")
+    dueDate.innerHTML = "Task to be done by 24 March 2023"
+}
+*/
